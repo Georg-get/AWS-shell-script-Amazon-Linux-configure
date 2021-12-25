a8b243445e1613016b4ac9302b6a76465d29403c
Johnimony <ujif12349@yandex.ru>
2021-12-25 16:09:10 +0300

    AWS

---
+++ AWS.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+###docker###
+yum update -y
+amazon-linux-extras install docker -y
+service docker start
+sudo usermod -aG docker ec2-user
+systemctl enable docker
+###docker-compose###
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+###vim htop git ###
+yum install vim htop git -y
+###git user###
+git config --global user.name "Johnimony"
+git config --global user.email ujif12349@yandex.ru
+###ssh-keygen###
+ssh-keygen -t rsa -b 4096 -C "ujif12349@yandex.ru" -f "home/ec2-user/.ssh/id_rsa" -q -P ""
+eval "$(ssh-agent -s)"
+ssh-add home/.ssh/id_rsa
+###dir prodject###
+mkdir /home/ec2-user/prodject
