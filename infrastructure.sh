324c259ddd57016e005da9cdec88ef3910446160
Johnimony <61118899+Georg-get@users.noreply.github.com>
2022-01-28 10:22:14 +0300

    Update infrastructure.sh

--- infrastructure.sh
+++ infrastructure.sh
@@ -3,7 +3,6 @@
 yum update -y
 amazon-linux-extras install docker -y
 service docker start
-sudo usermod -aG docker ec2-user
 systemctl enable docker
 ###docker-compose###
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -11,8 +10,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 ###vim htop git ###
 yum install vim htop git -y
 ###git user###
-git config --global user.name "Johnimony"
-git config --global user.email ujif12349@yandex.ru
+#git config --global user.name "Johnimony"
+#git config --global user.email ujif12349@yandex.ru
 ###ssh-keygen###
 ssh-keygen -t rsa -b 4096 -C "ujif12349@yandex.ru" -f "/home/ec2-user/.ssh/id_rsa" -q -P ""
 eval "$(ssh-agent -s)"
@@ -22,6 +21,8 @@ ssh-add /home/ec2-user/.ssh/id_rsa
 ###install gitlab runner ###
 curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh" | sudo bash
 sudo yum install gitlab-runner -y
+###add ec2-user and gitlab-runner to docker group
+sudo usermod -aG docker ec2-user gitlab-runner
 ###git clone ###
 git clone https://gitlab.com/Georg-get/coffe.git
 ###docker-compose start###
